4892877a633be9f74165a46b5143a4f91696626c
Shifa Gafoor A <shifagafoorncas@gmail.com>
2025-01-23 20:05:26 +0530

    .

---
+++ script.js
@@ -0,0 +1,18 @@
+function character(number) {
+    const char = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+    const base = char.length;
+  
+    number -= 1; 
+    if (number < 0) return "1"; 
+  
+    let result = "";
+    while (number >= base) {
+      result = char[number % base] + result;
+      number = Math.floor(number / base) - 1; 
+    }
+    result = char[number] + result;
+  
+    return result;
+  }
+
+ console.log( character(1400))
